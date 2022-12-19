2aed0bc523ac751b3b80447e3a954dcb1d1caf82
kadimakeith <keithkadima@gmail.com>
2022-12-19 08:15:20 -0800

    init repo + complete tasks

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    quotient = None
+    try:
+        quotient = a / b
+        print("Inside result: {}".format(quotient))
+    except(TypeError, ZeroDivisionError):
+        print("Inside result: {}".format(quotient))
+    finally:
+        return quotient
