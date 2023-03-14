d347cc42c2a81789385e4a291434a1227d72b517
kadimakeith <keithkadima@gmail.com>
2023-03-14 18:35:49 +0300

    .js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -2,12 +2,15 @@
 const myObject = {
     type: 'object',
     value: 12
-  };
-  console.log(myObject);
-  myObject.incr = function () {
+};
+
+console.log(myObject);
+
+myObject.incr = function () {
     this.value++;
-  };
-  myObject.incr();
+};
+
+myObject.incr();
   console.log(myObject);
   myObject.incr();
   console.log(myObject);
