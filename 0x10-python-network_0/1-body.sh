7f210428e24ecf8a0421e8dda7771bff9ae38117
kadimakeith <keithkadima@gmail.com>
2023-03-30 03:36:46 -0700

    cUrl to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# send a GET request to an URL with curl, and display the body of the response
+# send a GET request to specified URL with curl, and display the body of the response
 curl -sL "$1"
